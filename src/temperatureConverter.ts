56ad8ee5b91a3233492b7063b25b5d4c0d9cc385
Hannah <hannahpun@gmail.com>
2023-12-26 17:06:19 +0800

    temperatureConverter

--- src/temperatureConverter.ts
+++ src/temperatureConverter.ts
@@ -1,7 +1,8 @@
 /**
  * 將攝氏溫度轉換為華氏溫度
- * @param celsius - 攝氏溫度
- * @returns 華氏溫度
+ * @param {number} celsius - 攝氏溫度
+ * @returns {number} 華氏溫度
  */
 export function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9) / 5 + 32;
 }
