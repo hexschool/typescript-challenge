a2ef6a91215d4c3f595006ad6d6a4902171e067a
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-01-02 23:45:16 +0800

    feat: if-else test

--- src/ageClassification.ts
+++ src/ageClassification.ts
@@ -12,4 +12,13 @@
  */
 export function classifyAge(age: number): string {
     // 請在此處寫下你的程式碼
+    if (age <= 12) {
+        return 'Child';
+    } else if (age <= 17) {
+        return 'Teenager';
+    } else if (age <= 64) {
+        return 'Adult';
+    } else {
+        return 'Senior';
+    }
 }
