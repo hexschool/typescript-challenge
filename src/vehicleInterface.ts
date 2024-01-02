f6d268327b262abca2ce4201686817eeeb069a54
james.hsueh77 <james.hsueh@titansoft.com.sg>
2024-01-03 00:43:23 +0800

    feat: vehicle tests

--- src/vehicleInterface.ts
+++ src/vehicleInterface.ts
@@ -3,6 +3,12 @@
  * 車輛介面應該包含 brand、model 和 year 三個屬性，分別為字串型別和數字型別。
  */
 
+interface Vehicle {
+    brand: string;
+    model: string;
+    year: number;
+}
+
 /**
  * 獲取車輛的詳細資訊
  * @param vehicle - 車輛物件，應該包含 brand、model 和 year 三個屬性
@@ -11,6 +17,7 @@
  * 輸入: { brand: 'Toyota', model: 'Corolla', year: 2020 }
  * 輸出: 'Toyota Corolla (2020)'
  */
-export function vehicleInfo(vehicle: /* 你的車輛介面 */): string {
+export function vehicleInfo(vehicle: Vehicle/* 你的車輛介面 */): string {
     // 在此實現函式
+    return `${vehicle.brand} ${vehicle.model} (${vehicle.year})`;
 }
