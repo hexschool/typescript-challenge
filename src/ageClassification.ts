7cab16b40b998920ef52180367a1e9ac00c62a7a
Alex <alex830216@gmail.com>
2024-01-27 15:54:03 +0800

    完成年紀分類測試

--- src/ageClassification.ts
+++ src/ageClassification.ts
@@ -11,5 +11,13 @@
  * @returns - 回傳年齡的分類結果
  */
 export function classifyAge(age: number): string {
-    // 請在此處寫下你的程式碼
+    if (age <= 12) {
+        return 'Child';
+    } else if (age >= 13 && age <= 17) {
+        return 'Teenager';
+    } else if (age >= 18 && age <= 64) {
+        return 'Adult';
+    } else {
+        return 'Senior';
+    }
 }
